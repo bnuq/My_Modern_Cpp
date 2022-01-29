bb63f2bfc9f7833d262ecc6928695daffd8a79a4
bnuq <31835672+bnuq@users.noreply.github.com>
2022-01-29 17:05:05 +0900

    section 11 study

---
+++ Sect_11/05_Copy.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <filesystem>
+
+int main()
+{
+	using namespace std::filesystem;
+
+	// 현재 디렉토리
+	path source{ current_path() };
+	// 디렉토리 추가
+	source /= "Sect_11";
+	// 현재 파일 추가
+	source /= "05_Copy_Utility.cpp";
+
+	path dest{ current_path() };
+	dest /= "Sect_11";
+	dest /= "05_Copy.cpp";
+
+	std::cout << source << std::endl;
+
+	std::ifstream input{ source };
+	if (!input.is_open())
+	{
+		std::cout << "Source file not found" << std::endl;
+		return -1;
+	}
+
+	std::ofstream output{ dest };
+
+	std::string line;
+	while (!std::getline(input, line).eof())
+	{
+		output << line << std::endl;
+	}
+
+	// 수동으로 파일 닫기
+	input.close();
+	output.close();
+}

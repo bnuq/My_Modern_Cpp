ebfb15d2fd19f05750964055de399a1462925d2a
bnuq <2astwis2@gmail.com>
2022-03-16 19:52:34 +0900

    to initializer list

--- CleanVersion/8_More_C++_Goodies/91 Initializer List.cpp
+++ CleanVersion/8_More_C++_Goodies/91 Initializer List.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
 #include <initializer_list>
 #include <cassert>
+
+// container class 에서 많이 도움이 된다
 class Bag {
-	int arr[10];
+	int arr[10]{};
 	int m_Size{};
 public:
-	//Provides uniform initialization support for braced list of elements as arguments
-	Bag(std::initializer_list<int> values) {
+	// 임의의 배열처럼, 개수를 알 수 없는 어떤 배열로 객체를 만들고 싶다 + Uniform Initialization 을 하고 싶다
+	// 임의의 배열 => initializer list 로 표현
+	// Provides uniform initialization support for braced list of elements as arguments
+	Bag(std::initializer_list<int> values) { // initializer list 를 받는 생성자
 		assert(values.size() < 10);
-		auto it = values.begin();
+		auto it = values.begin(); // initializer list iterator
 		while (it != values.end()) {
 			Add(*it);
 			++it;
@@ -28,6 +32,8 @@ public:
 		return m_Size;
 	}
 };
+
+// 크기를 알 수 없는 배열을 넘겨받는 것과 같다
 void Print(std::initializer_list<int> values) {
 	//auto it = values.begin();
 	//while (it != values.end()) {
@@ -37,26 +43,39 @@ void Print(std::initializer_list<int> values) {
 		std::cout << x << " ";
 	}
 }
+
+
+
+
 int main() {
+	// Uniform Initialization
 	int x{ 0 };
 	float y{ 3.1f };
 	int arr[5]{ 3,1,3,8,2 };
 	std::string s{ "Hello C++" };
+
+
+	// Initializer_List => 배열처럼 여러 값들을 저장한다
 	std::initializer_list<int> data = { 1,2,3,4 };
+	// Uniform Initialization 이 아닌 데서, 갑자기 쓰이는 중괄호 => initializer list
 	//Braced list of elements automatically creates an initializer_list object
 	auto values = { 1,2,3,4 };
 
-	//Bag b{ 3,1,8 };
-	////b.Add(3);
-	////b.Add(1);
-	////b.Add(8);
-	//for (int i = 0; i < b.GetSize(); ++i) {
-	//	std::cout << b[i] << " "; 
-	//}
+	
+	
+	Bag b{ 3,1,8 };
+	//b.Add(3);
+	//b.Add(1);
+	//b.Add(8);
+	for (int i = 0; i < b.GetSize(); ++i) {
+		std::cout << b[i] << " "; 
+	}
 
-	//Print({ 8,2,6,7 });
+	// Print({ 8,2,6,7 });
 	for (auto x : { 8,2,6,7 }) {
 		std::cout << x << " ";
 	}
+
+
 	return 0;
 }
